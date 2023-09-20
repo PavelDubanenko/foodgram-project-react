d75704c947b81f6892de8fc17f1dc80733744994
morkva007 <morkovka007.89@mail.ru>
2023-09-20 12:32:15 +0300

    delete workflow

--- backend/createsuperuser.sh
+++
@@ -1,1 +0,0 @@
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@mail.ru').exists() or User.objects.create_superuser('admin@mail.ru','admin','Administrations','Admin','Qwdfg145!')" | python manage.py shell
